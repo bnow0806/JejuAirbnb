b36968fabb0abfc9c8f637f155a063eeef45fbbc
smilejakdu <ash982416@gmail.com>
2023-07-09 19:51:21 +0900

    fir commit

---
+++ src/main/java/com/example/jejuairbnb/JejuAirBnbApplication.java
@@ -0,0 +1,13 @@
+package com.example.jejuairbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejuAirBnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejuAirBnbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jejuairbnb/JejuAirBnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jejuairbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejuAirBnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
