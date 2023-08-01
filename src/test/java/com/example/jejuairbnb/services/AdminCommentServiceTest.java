d2512091f3d10ba063214f2894ce17fe4d408655
bnow0806 <guswp1993@naver.com>
2023-08-01 22:06:25 +0900

    CommentServiceTest File Format

--- src/test/java/com/example/jejuairbnb/services/AdminCommentServiceTest.java
+++ src/test/java/com/example/jejuairbnb/services/AdminCommentServiceTest.java
@@ -1,4 +1,53 @@
 package com.example.jejuairbnb.services;
 
+import com.example.jejuairbnb.adminServices.AdminCommentService;
+import com.example.jejuairbnb.repository.ICommentRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
 public class AdminCommentServiceTest {
+    @MockBean
+    private ICommentRepository commentRepository;
+
+    @Autowired
+    private AdminCommentService adminCommentService;
+
+    @Test
+    public void testCreateComment() {
+    }
+
+    @Test
+    public void testEnrollCommentToProduct() {
+    }
+
+    @Test
+    public void testCommentDescriptionLength() {
+    }
+
+    @Test
+    public void testUpdateComment() {
+    }
+
+    @Test
+    public void testUpdateCommentUpdatedAt() {
+    }
+
+    @Test
+    public void testDeleteComment() {
+    }
+
+    @Test
+    public void testDeleteCommentFromProduct() {
+    }
+
+    @Test
+    public void testFindCommentbyDescription() {
+    }
+
+    @Test
+    public void testReadCommentbyPagenationAndDescription() {
+    }
 }
--- src/test/java/com/example/jejuairbnb/services/CommentServiceTest.java
+++ src/test/java/com/example/jejuairbnb/services/CommentServiceTest.java
@@ -1,4 +1,29 @@
 package com.example.jejuairbnb.services;
 
+import com.example.jejuairbnb.repository.ICommentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+
+@SpringBootTest
 public class CommentServiceTest {
+
+    @MockBean
+    private ICommentRepository commentRepository;
+
+    private CommentService commentService;
+
+    @BeforeEach
+    public void setup() {
+        commentService = new CommentService(commentRepository);
+    }
+
+    @Test
+    public void testFindOneCommentById() {
+
+    }
+
+
 }
